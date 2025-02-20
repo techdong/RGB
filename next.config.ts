b212e3c820238efe7956e95969eb3230549eb6a8
Kako <kako@MBP25.local>
2025-02-20 01:22:03 +0000

    Initial commit for RGB project

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/RGB",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
