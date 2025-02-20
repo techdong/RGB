728316fe6ad2173066f5731db4e4ff1fb97a54b6
Kako <kako@MBP25.local>
2025-02-20 13:30:08 +0000

    Revert changes for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/RGB",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
